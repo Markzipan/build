7ddfa7901c4951a76af6989f4a1c9b76ed12e58d
MarkZ <markzipan@google.com>
2026-04-28 17:04:57 -0700

    FES + Build Daemon: Handle FES compile requests across rebuilds and expressions.

--- builder_pkgs/build_modules/lib/src/workers.dart
+++ builder_pkgs/build_modules/lib/src/workers.dart
@@ -3,6 +3,7 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'dart:async';
+import 'dart:convert';
 import 'dart:io';
 import 'dart:math' show min;
 
@@ -135,16 +136,121 @@ final frontendServerProxyDriverResource = Resource<FrontendServerProxyDriver>(
 
 PersistentFrontendServer? __persistentFrontendServer;
 
-/// Manages a single persistent instance of the Frontend Server targeting DDC.
+/// Returns the running instance of the PersistentFrontendServer, if any.
+PersistentFrontendServer? get persistentFrontendServer =>
+    __persistentFrontendServer;
+
+/// Starts a single persistent instance of the Frontend Server targeting DDC.
+///
+/// Also starts a socket server to listen for expression evaluation requests
+/// from the build daemon.
+Future<PersistentFrontendServer> startFrontendServerWorker() async {
+  if (__persistentFrontendServer != null) return __persistentFrontendServer!;
+
+  final fes = await PersistentFrontendServer.start(
+    sdkRoot: sdkDir,
+    fileSystemRoot: scratchSpace.tempDir.uri,
+    packagesFile: scratchSpace.tempDir.uri.resolve(packagesFilePath),
+  );
+
+  // Start socket server to receive expression eval requests from the build daemon.
+  final server = await ServerSocket.bind(InternetAddress.loopbackIPv4, 0);
+  final fesWorkerPortFile = File(
+    p.join(Directory.current.path, fesWorkerPortPath),
+  );
+  fesWorkerPortFile.createSync(recursive: true);
+  fesWorkerPortFile.writeAsStringSync('${server.port}');
+
+  _frontendServerProxyDriver.init(fes);
+  server.listen(
+    (socket) =>
+        _handleWorkerConnection(socket, _frontendServerProxyDriver, fes),
+  );
+  return __persistentFrontendServer = fes;
+}
+
+/// Handles a socket connection from the build daemon to the Frontend Server
+/// worker.
+///
+/// Also handles 'JSON_INPUT' protocol requests from the Frontend Server.
+/// See: `pkg/frontend_server/lib/frontend_server.dart` in the Dart SDK.
+void _handleWorkerConnection(
+  Socket socket,
+  FrontendServerProxyDriver driver,
+  PersistentFrontendServer fes,
+) {
+  socket
+      .cast<List<int>>()
+      .transform(utf8.decoder)
+      .transform(const LineSplitter())
+      .listen((line) async {
+        try {
+          final request = json.decode(line);
+          if (request case {
+            'type': 'EvaluateExpressionRequest',
+            'libraryUri': final String libraryUri,
+            'scriptUri': final String scriptUri,
+            'line': final int line,
+            'column': final int column,
+            'jsModules': final Map jsModules,
+            'jsFrameValues': final Map jsFrameValues,
+            'moduleName': final String moduleName,
+            'expression': final String expression,
+          }) {
+            final output = await driver.compileExpressionToJs(
+              libraryUri: libraryUri,
+              scriptUri: scriptUri,
+              line: line,
+              column: column,
+              jsModules: Map<String, String>.from(jsModules),
+              jsFrameValues: Map<String, String>.from(jsFrameValues),
+              moduleName: moduleName,
+              expression: expression,
+            );
+            socket.writeln(
+              json.encode({
+                'outputFilename': output?.outputFilename,
+                'errorCount': output?.errorCount,
+                'sources': output?.sources.map((u) => u.toString()).toList(),
+                'expressionData': () {
+                  // If the output contains expression data, encode it directly
+                  // in the response. Otherwise, if its output is written to
+                  // 'outputFilename'.
+                  if (output?.expressionData != null) {
+                    return base64.encode(output!.expressionData!);
+                  } else if (output?.outputFilename != null) {
+                    final file = File(output!.outputFilename);
+                    if (file.existsSync()) {
+                      return base64.encode(file.readAsBytesSync());
+                    }
+                  }
+                  return null;
+                }(),
+                'errorMessage': output?.errorMessage,
+              }),
+            );
+          } else if (request case {
+            'type': 'READ_OUTPUT_FILE',
+            'path': final String path,
+          }) {
+            final content = await fes.readOutputFile(path);
+            socket.writeln(json.encode({'content': content}));
+          }
+        } catch (e) {
+          socket.writeln(json.encode({'error': e.toString()}));
+        }
+      });
+}
+
 final persistentFrontendServerResource = Resource<PersistentFrontendServer>(
-  () async =>
-      __persistentFrontendServer ??= await PersistentFrontendServer.start(
-        sdkRoot: sdkDir,
-        fileSystemRoot: scratchSpace.tempDir.uri,
-        packagesFile: scratchSpace.tempDir.uri.resolve(packagesFilePath),
-      ),
+  () async => await startFrontendServerWorker(),
   beforeExit: () async {
     await __persistentFrontendServer?.shutdown();
     __persistentFrontendServer = null;
+
+    final fesWorkerPortFile = File(
+      p.join(Directory.current.path, fesWorkerPortPath),
+    );
+    if (fesWorkerPortFile.existsSync()) fesWorkerPortFile.deleteSync();
   },
 );
