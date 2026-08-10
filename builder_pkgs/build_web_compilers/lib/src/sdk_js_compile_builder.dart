edbfb873984a4445a548c59dd0e4b79e04a496d7
MarkZ <markzipan@google.com>
2026-08-10 16:30:08 -0700

    Remove DDC JIT snapshots from build_web_compilers

--- builder_pkgs/build_web_compilers/lib/src/sdk_js_compile_builder.dart
+++ builder_pkgs/build_web_compilers/lib/src/sdk_js_compile_builder.dart
@@ -107,27 +107,11 @@ Future<void> _createDevCompilerModule(
 
   ProcessResult result;
   try {
-    // Use standalone process instead of the worker due to
-    // https://github.com/dart-lang/sdk/issues/49441
-    var snapshotPath = p.join(
-      sdkDir,
-      'bin',
-      'snapshots',
-      'dartdevc_aot.dart.snapshot',
-    );
     final execSuffix = Platform.isWindows ? '.exe' : '';
-    var dartPath = p.join(sdkDir, 'bin', 'dartaotruntime$execSuffix');
-    if (!File(snapshotPath).existsSync()) {
-      snapshotPath = p.join(
-        sdkDir,
-        'bin',
-        'snapshots',
-        'dartdevc.dart.snapshot',
-      );
-      dartPath = p.join(sdkDir, 'bin', 'dart$execSuffix');
-    }
+    final dartPath = p.join(sdkDir, 'bin', 'dart$execSuffix');
     result = await Process.run(dartPath, [
-      snapshotPath,
+      'compile',
+      'js-dev',
       '--multi-root-scheme=org-dartlang-sdk',
       '--modules=${ddcLibraryBundle ? 'ddc' : 'amd'}',
       if (canaryFeatures || ddcLibraryBundle) '--canary',
